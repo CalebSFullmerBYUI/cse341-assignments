c50a8038edfac3ea0aeff933f8cebf74b5e882ac
CalebSFullmerBYUI <calebsf@byui.edu>
2021-04-22 14:26:52 -0600

    Prove 01 (without special character formatting)
    The primary submission for prove 01. Adds a page with two form text inputs. When the inputs are submitted, a page showing a concatenation of the given inputs is displayed.
    
    Special characters are not currently handled.

---
+++ cse341-Week01/routes/prove01.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+// Get default data for page.
+router.get('/', (req, res, next) => {
+    //res.render('/pages/prove01Input');
+    res.render('pages/prove01Input',{
+        path: '/prove01Input'
+    });
+});
+
+
+// Parse form data and render response page.
+// Form data will be concatenated.
+router.post('/responseData', (req, res, next)=> {
+    const incomingData = []
+    // Get incoming data.
+    req.on('data', (data)=> {
+        incomingData.push(data);
+    });
+
+    return req.on('end', () => {
+        // This does not handle special characters. With my present knowledge,
+        // it seems those chars must be handled individually.
+        parsedData = Buffer.concat(incomingData).toString();
+        parsedData = parsedData.split('&');
+
+        // Get input1 and input2 and concatenate them.
+        concatText = parsedData[0].split('=')[1].split('+').join(' ') + parsedData[1].split('=')[1].split('+').join(' ');
+
+        // Render output page.
+        res.render('pages/prove01Output',{
+            path: '/prove01Output',
+            // If nothing was entered, display a title indicating that.
+            title: (concatText) === '' ? 'Nothing Entered' : 'Your Concatenated Text',
+            concatenatedText: concatText
+        });
+        res.end();
+    });
+});
+
+
+module.exports = router;
