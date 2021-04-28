614a8e034d6a9dc2d7ef94f5ea7dd606208efc4e
CalebSFullmerBYUI <calebsf@byui.edu>
2021-04-27 20:05:58 -0600

    Team Activity 2

--- cse341-Week01/routes/ta02.js
+++ cse341-Week01/routes/ta02.js
@@ -1,14 +1,58 @@
 //TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself! 
+// Remember, you can make more of these placeholders yourself!
+
+let usernamesArray = ["tester1", "tester2"];
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.render('pages', { 
+    res.render('pages/ta02', { 
         title: 'Team Activity 02', 
-        path: '/ta02', // For pug, EJS 
+        path: 'ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        usernames: usernamesArray
+    });
+});
+
+router.post('/addUser',(req, res, next) => {
+    const incomingData = [];
+    req.on('data', (data) => {
+        incomingData.push(data);
+    });
+
+    req.on('end', () => {
+        data = Buffer.concat(incomingData).toString();
+        data = data.split('=')[1].replace('+', ' ');
+        usernamesArray.push(data);
+
+        res.redirect('/ta02');
+    });
+});
+
+
+router.post('/removeUser',(req, res, next) => {
+    const incomingData = [];
+    req.on('data', (data) => {
+        incomingData.push(data);
+    });
+
+    req.on('end', () => {
+        data = Buffer.concat(incomingData).toString();
+        data = data.split('=')[1].replace('+', ' ');
+        for (user of usernamesArray) {
+            console.log(data);
+            console.log(user);
+            console.log(user == data);
+            console.log(user === data);
+            if (user == data)
+            {
+                usernamesArray.splice(usernamesArray.indexOf(user), 1);
+            }
+        }
+
+        res.redirect('/ta02');
     });
 });
 
